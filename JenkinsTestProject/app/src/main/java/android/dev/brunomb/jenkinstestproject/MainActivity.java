1a3f08f67c5cee88012123d03d97f1112af0b60f
Bruno Brandão <bruno.brandao@embedded.ufcg.edu.br>
2016-06-28 11:06:50 -0300

    Mocking some errors and tests automatic build :zap:

--- JenkinsTestProject/app/src/main/java/android/dev/brunomb/jenkinstestproject/MainActivity.java
+++ JenkinsTestProject/app/src/main/java/android/dev/brunomb/jenkinstestproject/MainActivity.java
@@ -3,11 +3,20 @@ package android.dev.brunomb.jenkinstestproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Parameter Number
+    public void checkStuleErrorExample(String param1, String param2, String param3, String param4, String param5, String param6,
+                                       String param7, String param8, String param9, String param10, String param11, String param12
+                                       ) {
     }
 }
