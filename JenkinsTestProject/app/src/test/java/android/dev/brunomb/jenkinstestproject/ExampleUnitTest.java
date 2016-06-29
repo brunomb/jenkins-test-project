3e1794b98a8b6c85e003bf3b35485a7ba0a39ec0
Bruno Brandão <bruno.brandao@embedded.ufcg.edu.br>
2016-06-29 16:06:23 -0300

    Adding some tests

--- JenkinsTestProject/app/src/test/java/android/dev/brunomb/jenkinstestproject/ExampleUnitTest.java
+++ JenkinsTestProject/app/src/test/java/android/dev/brunomb/jenkinstestproject/ExampleUnitTest.java
@@ -8,8 +8,21 @@ import static org.junit.Assert.*;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void string_test() throws Exception {
+        assertEquals("nothing","nothing");
+        assertNotEquals("nothing","everything");
+    }
+
+    @Test
+    public void int_isCorrect() throws Exception {
+        assertEquals(1,1);
+        assertNotEquals(1,42);
     }
 }
